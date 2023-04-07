1cc3d9551cc6a2fee15d48e8b0f752b8c8d32197
Ariton <aritonsefedini@icloud.com>
2023-04-07 17:13:07 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namesapce :api, defaults: {format: :json} do
+    resources :users
+    resources :session
+  end
+
+  root 'static_pages#root'
 end
