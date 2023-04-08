4ef88f55c16e9932aba08fad7e4243ca65936e44
Ariton <aritonsefedini@icloud.com>
2023-04-08 14:22:02 -0400

    Root reducer, store

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,16 @@
+import { combinedReducers } from 'redux';
+import entitiesReducer from './entitites_reducer';
+import sessionErrorsReducer from './session_errors_reducer';
+import sessionReducer from './session_reducer';
+import usersReducer from './users_reducer';
+
+const rootReducer = combinedReducers({
+    entitites: entitiesReducer,
+    session: sessionReducer,
+    users: usersReducer,
+    errors: {
+        session: sessionErrorsReducer
+    }
+});
+
+export default rootReducer;
