52f7be1e1ea05833e31a540baa654bda3de7635d
Ariton <aritonsefedini@icloud.com>
2023-04-08 14:10:48 -0400

    Edit users reducer, create entities reducer

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,3 +1,5 @@
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from "../actions/session_actions";
+import { RECEIVE_ALL_USERS } from "../actions/user_actions";
 
 
 const usersReducer = (oldState = {}, action) => {
@@ -5,5 +7,17 @@ const usersReducer = (oldState = {}, action) => {
     let nextState = Object.assign({}, oldState);
 
     switch (action.type) {
+        case RECEIVE_ALL_USERS:
+            return action.users;
+        case RECEIVE_CURRENT_USER:
+            nextState[action.user.id] = action.user;
+            return nextState;
+        case LOGOUT_CURRENT_USER:
+            delete nextState[action.user.id];
+            return nextState;
+        default:
+            return oldState;
     }
 }
+
+export default usersReducer;
