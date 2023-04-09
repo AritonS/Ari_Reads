3ff8906b981dacaf8bd2a73f3265cb760b685334
Ariton <aritonsefedini@icloud.com>
2023-04-09 11:17:23 -0400

    small edits

--- frontend/ari_reads.jsx
+++ frontend/ari_reads.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from "./components/root";
@@ -7,10 +8,10 @@ document.addEventListener("DOMContentLoaded", () => {
     let store;
     if (window.currentUser) {
         const preloadedState = {
+            session: { id: window.currentUser.id },
             entities: {
                 users: { [window.currentUser.id]: window.currentUser }
-            },
-            session: { id: window.currentUser.id }
+            }
         };
         store = configureStore(preloadedState);
         delete window.currentUser;
@@ -18,12 +19,6 @@ document.addEventListener("DOMContentLoaded", () => {
         store = configureStore();
     }
     
-    // TESTING START
-    // window.getState = store.getState;
-    // window.dispatch = store.dispatch;
-    // TESTING END
-    
     const root = document.getElementById("root");
     ReactDOM.render(<Root store={store}/>, root);
-    
 });
