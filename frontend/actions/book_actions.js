8434b42e952a83667c68f38020be3a5c7d42e1da
Ariton <aritonsefedini@icloud.com>
2023-04-10 10:39:47 -0400

    edit actions

--- frontend/actions/book_actions.js
+++ frontend/actions/book_actions.js
@@ -4,47 +4,48 @@ export const RECEIVE_BOOKS = 'RECEIVE_BOOKS';
 export const RECEIVE_BOOK = 'RECEIVE_BOOK';
 export const REMOVE_BOOK = 'REMOVE_BOOK';
 
-export const receiveBooks = books => ({
-    type: RECEIVE_BOOKS,
-    books
-});
+const receiveBooks = books => {
+    return {
+        type: RECEIVE_BOOKS,
+        books
+    }
+}
 
-export const receiveBook = book => ({
-    type: RECEIVE_BOOK,
-    book
-});
+const receiveBook = book => {
+    return {
+        type: RECEIVE_BOOK,
+        book
+    }
+}
 
-export const removeBook = bookId => ({
-    type: REMOVE_BOOK,
-    bookId
-});
+const removeBook = bookId => {
+    return {
+        type: REMOVE_BOOK,
+        bookId
+    }
+}
 
-export const fetchBooks = (userId, listId) => dispatch => (
-    BookApiUtil.fetchBooks(userId, listId).then(books =>
-        dispatch(receiveBooks(books))
-    )
-);
+export const fetchBooks = listId => dispatch => {
+    return BookApiUtil.fetchBooks(listId)
+        .then(books => dispatch(receiveBooks(books)))
+}
 
-export const fetchBook = (userId, listId, bookId) => dispatch => (
-    BookApiUtil.fetchBook(userId, listId, bookId).then(book =>
-        dispatch(receiveBook(book))
-    )
-);
+export const fetchBook = (listId, bookId) => dispatch => {
+    return BookApiUtil.fetchBook(listId, bookId)
+        .then(book => dispatch(receiveBook(book)))
+}
 
-export const createBook = (userId, listId, book) => dispatch => (
-    BookApiUtil.createBook(userId, listId, book).then(book =>
-        dispatch(receiveBook(book))
-    )
-);
+export const createBook = (listId, book) => dispatch => {
+    return BookApiUtil.createBook(listId, book)
+        .then(createdBook => dispatch(receiveBook(createdBook)))
+}
 
-export const updateBook = (userId, listId, book) => dispatch => (
-    BookApiUtil.updateBook(userId, listId, book).then(book =>
-        dispatch(receiveBook(book))
-    )
-);
+export const updateBook = (listId, book) => dispatch => {
+    return BookApiUtil.updateBook(listId, book)
+        .then(updatedBook => dispatch(receiveBook(updatedBook)))
+}
 
-export const deleteBook = (userId, listId, bookId) => dispatch => (
-    BookApiUtil.deleteBook(userId, listId, bookId).then(() =>
-        dispatch(removeBook(bookId))
-    )
-);
+export const deleteBook = (listId, bookId) => dispatch => {
+    return BookApiUtil.deleteBook(listId, bookId)
+        .then(() => dispatch(removeBook(bookId)))
+}
--- frontend/actions/list_actions.js
+++ frontend/actions/list_actions.js
@@ -4,47 +4,48 @@ export const RECEIVE_LISTS = 'RECEIVE_LISTS';
 export const RECEIVE_LIST = 'RECEIVE_LIST';
 export const REMOVE_LIST = 'REMOVE_LIST';
 
-export const receiveLists = lists => ({
-    type: RECEIVE_LISTS,
-    lists
-});
+const receiveLists = lists => {
+    return {
+        type: RECEIVE_LISTS,
+        lists
+    }
+}
 
-export const receiveList = list => ({
-    type: RECEIVE_LIST,
-    list
-});
+const receiveList = list => {
+    return {
+        type: RECEIVE_LIST,
+        list
+    }
+}
 
-export const removeList = listId => ({
-    type: REMOVE_LIST,
-    listId
-});
+const removeList = listId => {
+    return {
+        type: REMOVE_LIST,
+        listId
+    }
+}
 
-export const fetchLists = userId => dispatch => (
-    ListApiUtil.fetchLists(userId).then(lists =>
-        dispatch(receiveLists(lists))
-    )
-);
+export const fetchLists = userId => dispatch => {
+    return ListApiUtil.fetchLists(userId)
+        .then(lists => dispatch(receiveLists(lists)))
+}
 
-export const fetchList = (userId, listId) => dispatch => (
-    ListApiUtil.fetchList(userId, listId).then(list =>
-        dispatch(receiveList(list))
-    )
-);
+export const fetchList = (userId, listId) => dispatch => {
+    return ListApiUtil.fetchList(userId, listId)
+        .then(list => dispatch(receiveList(list)))
+}
 
-export const createList = (userId, list) => dispatch => (
-    ListApiUtil.createList(userId, list).then(list =>
-        dispatch(receiveList(list))
-    )
-);
+export const createList = (userId, list) => dispatch => {
+    return ListApiUtil.createList(userId, list)
+        .then(createdList => dispatch(receiveList(createdList)))
+}
 
-export const updateList = (userId, list) => dispatch => (
-    ListApiUtil.updateList(userId, list).then(list =>
-        dispatch(receiveList(list))
-    )
-);
+export const updateList = (userId, list) => dispatch => {
+    return ListApiUtil.updateList(userId, list)
+        .then(updatedList => dispatch(receiveList(updatedList)))
+}
 
-export const deleteList = (userId, listId) => dispatch => (
-    ListApiUtil.deleteList(userId, listId).then(() =>
-        dispatch(removeList(listId))
-    )
-);
+export const deleteList = (userId, listId) => dispatch => {
+    return ListApiUtil.deleteList(userId, listId)
+        .then(() => dispatch(removeList(listId)))
+}
