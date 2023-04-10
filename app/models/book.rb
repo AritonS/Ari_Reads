c73a3123dc1813020f914940dd2b45184d72b8e3
Ariton <aritonsefedini@icloud.com>
2023-04-09 21:17:25 -0400

    Update list and book models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,10 @@
 class Book < ApplicationRecord
+  enum status: { want_to_read: 0, currently_reading: 1, finished_reading: 2 }
   belongs_to :list
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :status, presence: true
+  validates :total_pages, numericality: { greater_than: 0 }, allow_nil: true
+  validates :current_page, numericality: { greater_than: 0 }, allow_nil: true
 end
