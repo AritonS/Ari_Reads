396da4ab6cf37b733c0636200b020071fe770d01
Ariton <aritonsefedini@icloud.com>
2023-04-09 22:08:02 -0400

    typo

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,10 +1,10 @@
-import { combinedReducers } from 'redux';
+import { combineReducers } from 'redux';
 import entitiesReducer from './entitites_reducer';
 import sessionErrorsReducer from './session_errors_reducer';
 import sessionReducer from './session_reducer';
 import usersReducer from './users_reducer';
 
-const rootReducer = combinedReducers({
+const rootReducer = combineReducers({
     entitites: entitiesReducer,
     session: sessionReducer,
     users: usersReducer,
